20424bbe41629365119a37e3505e210094a35285
NjokiNdichu <rejoicenjoki0@gmail.com>
2023-03-29 17:07:39 +0300

    task-3

---
+++ task3.py
@@ -0,0 +1,18 @@
+num=input("Enter your phone number: ")
+if (num[0]=="0" and num[1]=="7"):
+    num[0]=="+" and num[1]=="2" and num[2]=="5" and num[3]=="4"
+    print(num)
+elif (num[0]=="0" and num[1]=="1"):
+    num[0]=="+" and num[1]=="2" and num[2]=="5" and num[3]=="4"
+    print(num)
+elif (num[0]=="7" and num[1]=="1"):
+    num[0]=="+" and num[1]=="2" and num[2]=="5" and num[3]=="4"
+    print(num)
+elif (num[0]=="2" and num[1]=="5" and num[2]=="4"):
+    num[0]=="+" and num[1]=="2" and num[2]=="5" and num[3]=="4"
+    print(num)
+else:
+    print("false")
+   
+
+
