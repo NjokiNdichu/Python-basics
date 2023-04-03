10f856e7fd14c9688a482a6d7219c07990c20483
NjokiNdichu <rejoicenjoki0@gmail.com>
2023-04-03 16:43:21 +0300

    summarytask

--- summarytask.py
+++ summarytask.py
@@ -3,15 +3,15 @@ width = float(input("Enter the width of the cuboid: "))
 height = float(input("Enter the height of the cuboid: "))
 
 
-def surface_area():
-    surfacearea=2 * (length * width + length * height + width * height)
-    print("The surface area is:", surfacearea)
-
-surface_area()
-
-def volume():
-    vol=length * width * height
-    print("The volume is:", vol)
+def surface_area(length,width,height):
+    return 2 * (length * width + length * height + width * height)
 
 
-volume()
+def volume(length,width,height):
+    return length * width * height
+
+
+sa=surface_area(length,width,height)
+vol=volume(length,width,height)
+print("The surface area is:", sa)
+print("The volume is:", vol)
